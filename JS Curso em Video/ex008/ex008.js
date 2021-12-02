afe0756a0f48996b0a10c4a7ea50038dccb92648
leochaguri <94688259+leochaguri@users.noreply.github.com>
2021-12-02 09:59:18 -0300

    att

---
+++ JS Curso em Video/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel =  60
+console.log(`A velocidade do seu carro é ${vel} Km/h`)
+if(vel > 60) { //condição simples
+    console.log(`Velocidade permitida ultrapassada, você foi multado!`)
+}
+console.log(`Dirija sempre com cinto de segurança.`)
