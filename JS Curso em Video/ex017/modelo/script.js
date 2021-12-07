c469c3eae088edf8c46516016366610f046fdaac
leochaguri <94688259+leochaguri@users.noreply.github.com>
2021-12-07 14:59:32 -0300

    att

---
+++ JS Curso em Video/ex017/modelo/script.js
@@ -0,0 +1,29 @@
+var num = document.getElementById('fnum')
+var lista = document.getElementById('flista')
+var res = document.getElementById('res')
+var valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    } 
+}
+
+function inLista(n,l) {
+    if (l.indexOf(Number(n)) != -1) {
+    return true
+} else{
+    return false
+}
+
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        window.alert('Tudo ok')
+    }else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
+    }
+}
